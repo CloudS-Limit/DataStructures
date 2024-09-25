062ed0213a56ab348371c192331b82baaf983a3e
CloudS-Limit <bi54nn@student.sunderland.ac.uk>
2024-09-25 15:01:47 +0100

    added class TVShow and a list option

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,12 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            Movie deadpool3;
+            deadpool3.title = "deadpool and wolverine";
+            deadpool3.releaseYear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
 
-            float someFloat = 3.14159f;
-            Console.WriteLine(someFloat.ToString("0.000"));
-            Console.ReadKey(); 
+            Console.WriteLine(deadpool3.title + "(" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
+
+            Console.ReadKey();
+            Console.WriteLine("\n\n");
+            Console.Clear();
+
+            List<TVShow> shows = new List <TVShow>();  
+            shows.Add(new TVShow("The Lord of the Rings: The Rings Of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVShow("the boys", 2019, 7.99, MovieRating._18));
+
+            for (int i = 0; i < shows.Count; i++)
+            {
+                Console.WriteLine(shows[i].ToString());
+            }
+            Console.ReadKey();
+        }
+
+        public enum MovieRating 
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18
+            
+        }
+
+        public struct Movie
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
         }
     }
 }
